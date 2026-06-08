60994e31c2870b7a42bfd8bddc6c4dbcd6334b58
Aarushi-Singh-19 <aarushisingh19102005@gmail.com>
2026-06-08 10:09:39 +0530

    Add Instagram connection fields to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,11 +2,45 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    instagramConnected: {
+      type: Boolean,
+      default: false,
+    },
+
+    instagramBusinessId: {
+      type: String,
+      default: null,
+    },
+
+    facebookPageId: {
+      type: String,
+      default: null,
+    },
+
+    accessToken: {
+      type: String,
+      default: null,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("User", userSchema);
