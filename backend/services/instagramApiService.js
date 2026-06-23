519c309fd62d6d921ff3c78dc530814877b6d8dc
Aarushi-Singh-19 <aarushisingh19102005@gmail.com>
2026-06-23 09:35:28 +0530

    testing

--- backend/services/instagramApiService.js
+++ backend/services/instagramApiService.js
@@ -3,19 +3,6 @@ const axios = require("axios");
 const GRAPH_VERSION = "v19.0";
 
 async function replyToComment({ accessToken, commentId, message }) {
-  const url = `https://graph.facebook.com/${GRAPH_VERSION}/${commentId}/replies`;
-
-  const res = await axios.post(url, null, {
-    params: {
-      message,
-      access_token: accessToken,
-    },
-  });
-
-  return res.data;
-}
-
-async function replyToComment({ accessToken, commentId, message }) {
   try {
     console.log("REPLY ATTEMPT");
     console.log("COMMENT ID:", commentId);
@@ -41,6 +28,25 @@ async function replyToComment({ accessToken, commentId, message }) {
 
     throw err;
   }
+}
+
+async function sendDM({ accessToken, igUserId, message }) {
+  const url = `https://graph.facebook.com/${GRAPH_VERSION}/${igUserId}/messages`;
+
+  const res = await axios.post(
+    url,
+    {
+      recipient: { id: igUserId },
+      message: { text: message },
+    },
+    {
+      params: {
+        access_token: accessToken,
+      },
+    }
+  );
+
+  return res.data;
 }
 
 module.exports = {
