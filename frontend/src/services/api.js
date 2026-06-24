d1b9147d38576153c628fb0023abcb1129bb1d7e
Aarushi-Singh-19 <aarushisingh19102005@gmail.com>
2026-06-24 11:33:02 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://insta-automation-slwr.onrender.com",
 });
-
 // Attach token automatically (for SaaS auth later)
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
